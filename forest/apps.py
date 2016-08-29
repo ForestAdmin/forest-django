1348a0e6ba748307ee20ad2e2e168d6da978e53d
Raphael De Giusti <raphael.degiusti@gmail.com>
2016-08-29 09:37:03 +0200

    Activate apimap send to server

--- forest/apps.py
+++ forest/apps.py
@@ -10,12 +10,11 @@ class ForestConfig(AppConfig):
     verbose_name = "My Forest Connector"
     def ready(self):
         apimap = api_map.generate()
-        # url = 'https://forestadmin-server.herokuapp.com'
-        # url += '/forest/apimaps'
-        # secret_key = '5135a1c7e29787b300f1f6326dfe8819b147aad72807e0f28ae9a82209e45076'
-        # headers = {
-            # 'forest-secret-key':  secret_key,
-            # 'Content-Type': 'application/json'
-        # }
-        # req = requests.post(url, data=json.dumps(apimap), headers=headers)
-        pass
+        url = 'https://forestadmin-server.herokuapp.com'
+        url += '/forest/apimaps'
+        secret_key = '5135a1c7e29787b300f1f6326dfe8819b147aad72807e0f28ae9a82209e45076'
+        headers = {
+            'forest-secret-key':  secret_key,
+            'Content-Type': 'application/json'
+        }
+        req = requests.post(url, data=json.dumps(apimap), headers=headers)
